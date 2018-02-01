5fd2d75eb0f8bd8f6f14bb65692bf0ad034353c4
Robert Docter <rd+git@figo.io>
2018-02-01 17:11:24 +0100

    feat: add sass

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,51 @@
 const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const ENTRY_DIR = path.resolve(__dirname, 'src')
+const BUILD_DIR = path.resolve(__dirname, 'dist')
 
 module.exports = {
-    entry: './src/index.tsx',
+    entry: path.join(ENTRY_DIR, 'index.js'),
     output: {
         filename: 'bundle-[hash].js',
-        path: path.join(__dirname, '/dist')
+        path: BUILD_DIR,
+        publicPath: '/'
     },
-    resolve: { extensions: ['.ts', '.tsx', '.js', '.json'] },
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader', options: { silent: true } },
-            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+            { // JS
+                test: /\.jsx?/,
+                include: ENTRY_DIR,
+                loader: 'babel-loader'
+            },
+            { // CSS/SASS
+                test: /\.(sass|scss|css)$/,
+                // include: [
+                //     path.join(__dirname, 'node_modules', 'bulma', 'bulma.sass'),
+                //     path.join(__dirname, 'src', 'client', 'styles')
+                // ],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader' },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                includePaths: [
+                                    path.join(__dirname, 'node_modules')
+                                ],
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: 'src/index.html' })
+        new HtmlWebpackPlugin({ template: 'src/assets/index.html' }),
+        new ExtractTextPlugin('main-[hash].css')
     ]
 };
